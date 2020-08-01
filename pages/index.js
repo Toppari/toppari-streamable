6ea0197de319d102f67d3198a5ef460543637e72
Toppari <oksanen.topias@gmail.com>
2020-08-01 17:14:12 +0300

    use html5 video element to show videos and some styling improvements

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,45 @@
-export default function Home({ videos }) {
-  const latestTen = videos.videos.slice(0, 9);
+import styles from '../styles/index.module.css';
 
+export default function Home({ videos }) {
   return (
-    <div style={{ margin: '10px' }}>
+    <div className={styles.wrapper}>
       <h1>Videoita yhteensä: {videos.total}</h1>
       <h3>Viimeisimmät 10 videota</h3>
-      {latestTen.map(({ file_id, url }) => (
-        <div key={file_id} style={{ margin: '10px' }}>
-          <a href={url} target='_blank'>
-            Katso video
-          </a>
-        </div>
-      ))}
+      {videos.videos.map(
+        ({ file_id, url, title, files, dynamic_thumbnail_url: poster }) => (
+          <div key={file_id}>
+            <h4>{title}</h4>
+            <video
+              controls
+              controlsList='nodownload'
+              width='720'
+              poster={poster}
+              preload='metadata'
+            >
+              {/* mp4-high is available after streamable has fully processed the video */}
+              {files['mp4-high'].url && (
+                <source
+                  src={`https:${files['mp4-high'].url}`}
+                  type='video/mp4'
+                />
+              )}
+              <source src={`https:${files.mp4.url}`} type='video/mp4' />
+              <p>
+                Selaimesi ei tue embed videoita. Tässä
+                <a
+                  style={{ margin: '3px' }}
+                  href={url}
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  linkki
+                </a>
+                videoon.
+              </p>
+            </video>
+          </div>
+        ),
+      )}
     </div>
   );
 }
@@ -29,7 +57,7 @@ export const getServerSideProps = async () => {
   };
 
   const loginResponse = await fetch('https://ajax.streamable.com/check', {
-    method: 'post',
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
@@ -42,13 +70,17 @@ export const getServerSideProps = async () => {
 
   const parsedCookies = parseCookies(loginResponse);
 
-  const videosResponse = await fetch('https://ajax.streamable.com/videos', {
-    method: 'GET',
-    credentials: 'include',
-    headers: {
-      cookie: parsedCookies,
+  // Only fetch latest 10 for now
+  const videosResponse = await fetch(
+    'https://ajax.streamable.com/videos?sort=date_added&sortd=DESC&count=10&page=1',
+    {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        cookie: parsedCookies,
+      },
     },
-  });
+  );
 
   const videos = await videosResponse.json();
 
